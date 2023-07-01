ac6c208bf8f38d7dcedf3e8cc908179ee65568f1
xangjong <csj7080@naver.com>
2023-07-01 15:17:06 +0900

    Add office-management source

---
+++ office-management/src/main/java/com/om/officemanagement/OfficeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.om.officemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficeManagementApplication.class, args);
+    }
+
+}
